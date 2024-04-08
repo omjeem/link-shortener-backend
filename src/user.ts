407adcaf8e5ec897c41e7322869d1973c250f7f5
Om Jee Mishra <93421142+om-jee-m@users.noreply.github.com>
2024-04-08 22:24:26 +0530

    signIn route update

--- src/user.ts
+++ src/user.ts
@@ -22,7 +22,7 @@ userRouter.post("/", async (req: any, res: any) => {
         return res.status(200).json(token);
     } catch (err) {
         return res.status(402).json({
-            message : "Email Already exists"
+            message: "Email Already exists"
         });
     }
 })
@@ -42,8 +42,14 @@ userRouter.get("/", async (req: any, res: any) => {
         if (response.rows.length === 0) {
             return res.status(403).json({ message: "User not found / Invalid Credentials" })
         }
+        const query2 = `select * from links where userid = $1;`
+        const values2 = [response.rows[0].id]
+        const links = await pool.query(query2, values2);
         const token = jwt.sign({ id: response.rows[0].id }, process.env.JWT_SECRET || "secret-key");
-        return res.status(200).json(token);
+        return res.status(200).json({
+            token,
+            links : links.rows
+        });
     } catch (err) {
         return res.send(err);
     }
